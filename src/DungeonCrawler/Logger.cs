977016658979088a2ed7feeca3cf646f3cd2e466
ankatus <antti.j.kataja@gmail.com>
2021-03-15 14:59:37 +0200

    Added basic logger

---
+++ src/DungeonCrawler/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace DungeonCrawler
+{
+    public class Logger
+    {
+        public static void Log(string message, string fileName)
+        {
+            var logFile = fileName + ".log";
+            var path = Directory.GetCurrentDirectory() + @"\" + logFile;
+            using var stream = File.AppendText(path);
+            stream.WriteLine(DateTime.Now.ToString("H:mm:ss", CultureInfo.InvariantCulture) + ": " + message);
+        }
+    }
+}
