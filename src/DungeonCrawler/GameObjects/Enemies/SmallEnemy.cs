e24c539c6398cad154d955ca73391715003f9559
ankatus <antti.j.kataja@gmail.com>
2021-04-30 16:38:37 +0300

    Renamed DefaultEnemy to SmallEnemy

---
+++ src/DungeonCrawler/GameObjects/Enemies/SmallEnemy.cs
@@ -0,0 +1,15 @@
+﻿using DungeonCrawler.Rooms;
+using Microsoft.Xna.Framework;
+
+namespace DungeonCrawler.GameObjects.Enemies
+{
+    public class SmallEnemy : Enemy
+    {
+        private const int WIDTH = 30;
+        private const int HEIGHT = 30;
+        public SmallEnemy(Room room, Vector2 position) : base(room, position, WIDTH, HEIGHT)
+        {
+
+        }
+    }
+}
